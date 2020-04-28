b9463adee2d9f39ef9e17877a53a0732b7c65da3
william.pennoyer <william.pennoyer@projectdrgn.com>
2020-04-28 23:39:54 +0200

    keccak before reimplementation of union.

--- app/keccak.py
+++ app/keccak.py
@@ -1,6 +1,6 @@
 import copy
-import io
 import ctypes
+import io
 
 rot = [
     [0, 36, 3, 41, 18],
@@ -39,26 +39,18 @@ rc_24 = [
 
 
 def roll_reg_left(arr, beg):
-    f = arr[beg] >> 7 & 0x01
-    for x in range(7):
-        i = beg + x
-        v1 = arr[i]
-        v2 = arr[i + 1]
-        t1 = 0xff & v1 << 1
-        t2 = 0xff & v2 >> 7
-        arr[i] = t1 ^ t2
-
-    l = 0xff & arr[beg + 7] << 1
-    arr[beg + 7] = l ^ f
+        arr[beg] =  ((arr[beg] << 1) | (arr[beg] >> 63))
 
 
-def rolln_reg_left(arr: bytearray, beg, nbits):
+def rolln_reg_left(arr, beg, nbits):
     for n in range(nbits):
         roll_reg_left(arr, beg)
 
+
 def chi_operation(dst, ai, b, r1, r2, r3):
     for i in range(8):
-        dst[ai+i] = b[r1+i] ^ (~b[r2+i] & b[r3+i])
+        dst[ai + i] = b[r1 + i] ^ (~b[r2 + i] & b[r3 + i])
+
 
 def dump_array(b: bytearray, n):
     for z in range(n):
@@ -67,8 +59,8 @@ def dump_array(b: bytearray, n):
         v = b[z]
         print(f"{v:02x}|", end="")
 
-
     print("")
+
 
 def dump_buffer(b: bytearray):
     for y in range(5):
@@ -81,6 +73,7 @@ def dump_buffer(b: bytearray):
                 print(f"{v:02x}|", end="")
             print("")
 
+
 class StateUnion(ctypes.Union):
     _fields_ = [("buffer", ctypes.c_ubyte * 200),
                 ("register", ctypes.c_ulonglong * 25)]
@@ -89,34 +82,27 @@ class StateUnion(ctypes.Union):
         self.buffer[0] = 0x01;
 
         for x in range(65):
-            self.register[0] = ((self.register[0]  << 1) | (self.register[0] >> 63))
+            self.register[0] = ((self.register[0] << 1) | (self.register[0] >> 63))
             dump_buffer(self.buffer)
             print("--------")
 
-class State():
-    # _fields_ = [("buffer", c_ubyte * 200),
-    #            ("register", c_ulonglong * 25)]
 
-    buffer = bytearray(200)
-    b = bytearray(200)
+class State(ctypes.Union):
+    _fields_ = [("buffer", ctypes.c_ubyte * 200),
+                ("register", ctypes.c_ulonglong * 25)]
 
-    def print(self):
-        for x in range(200):
-            if x % 8 == 0:
-                print("", end="|")
-            print(hex(self.buffer[x]), end="|")
-        print("")
-        for x in range(200):
-            value = self.buffer[x]
-            v1 = format(value, '#010b')
-            print(v1, end="|")
+def rol(self, reg_id):
+    roll_reg_left(self.buffer, reg_id * 8)
 
-    def rol(self, reg_id):
-        roll_reg_left(self.buffer, reg_id * 8)
 
-    def roln(self, reg_id, times):
-        for x in range(times):
-            self.rol(reg_id)
+def roln(self, reg_id, times):
+    for x in range(times):
+        self.rol(reg_id)
+
+class B(ctypes.Union):
+    _fields_ = [("bite", ctypes.c_ubyte * 40),
+                ("register", ctypes.c_ulonglong * 5)]
+
 
 class Keccak:
     # 25 * 64 = 25 * 8 * 8 = 200 * 8 = 1600
@@ -124,48 +110,46 @@ class Keccak:
     RATE = 136
     STATE_SIZE = 200
     state = State()
+    b = State()
 
     def _theta(self):
-        dump_buffer(self.state.buffer)
-        c1 = bytearray(40)
+        print("_theta begin")
+        #dump_buffer(self.state.buffer)
+        c1 = B()
         for x in range(5):
             for y in range(5):
                 for z in range(8):
                     ri = x * 8 + y * 40 + z
                     ci = z + x * 8
-                    c1[ci] ^= self.state.buffer[ri]
+                    c1.bite[ci] ^= self.state.buffer[ri]
 
-        dump_array(c1, 40)
-        print("")
-
-        c2 = copy.deepcopy(c1)
+        c2 = B()
+        c2.bite = copy.deepcopy(c1.bite)
         for x in range(5):
-            xi = x * 8
-            rolln_reg_left(c2, xi, 1)
+            rolln_reg_left(c2.register, x, 1)
 
-        dump_array(c2, 40)
-        print("")
-
-        d = bytearray(40)
+        d = B()
         for x in range(5):
             x1 = (x + 4) % 5
             x2 = (x + 1) % 5
             for z in range(8):
-                d[x * 8 + z] = c1[x1 * 8 + z] ^ c2[x2 * 8 + z]
-
-        dump_array(d, 40)
-        print("")
+                d.bite[x * 8 + z] = c1.bite[x1 * 8 + z] ^ c2.bite[x2 * 8 + z]
 
         for x in range(5):
             for y in range(5):
                 for z in range(8):
                     si = x * 8 + y * 40 + z
                     di = z + x * 8
-                    self.state.buffer[si] ^= d[di]
+                    self.state.buffer[si] ^= d.bite[di]
 
+
+        dump_buffer(self.state.buffer)
+        print("_theta end")
     def _rho_pi(self):
-        b = self.state.b
-        for x in range(200): b[x]=0x0
+        print("_rho_pi begin")
+
+        b = self.b
+        for x in range(200): b.buffer[x] = 0x0
 
         for x in range(5):
             for y in range(5):
@@ -173,36 +157,42 @@ class Keccak:
                 by = (2 * x + 3 * y) % 5
                 rv = rot[x][y]
                 #print(f"({x},{y})=>({bx},{by}) : {rv}")
-                b_i = bx * 8 + by * 40
-                for z in range(8):
-                    b[b_i + z] = self.state.buffer[x * 8 + y * 40 + z]
+                b_i = by + bx * 5
+                i = x + y * 5
+                b.register[b_i] = self.state.register[i]
 
-                rolln_reg_left(b, b_i, rv)
+                rolln_reg_left(b.register, b_i, rv)
+
+        dump_buffer(b.buffer)
+        print("_rho_pi end")
 
     def _chi(self):
+        print("_chi begin")
         a = self.state.buffer
-        b = self.state.b
+        b = self.b.buffer
         for y in range(5):
             for x in range(5):
-                a_i = x*8 + y*40
+                a_i = x * 8 + y * 40
                 b_i = a_i
-                b2_i = (x+1)%5*8 + (y*40)
-                b3_i = (x+2)%5*8 + (y*40)
+                b2_i = (x + 1) % 5 * 8 + (y * 40)
+                b3_i = (x + 2) % 5 * 8 + (y * 40)
                 chi_operation(a, a_i, b, b_i, b2_i, b3_i)
 
+        dump_buffer(a)
+        print("_chi end")
     def _iota(self, rnd):
         a = self.state.buffer
         for i in range(8):
-            a[i] =  a[i] ^ rc_24[rnd][i]
+            a[i] = a[i] ^ rc_24[rnd][i]
 
     def _rounds(self):
         for x in range(24):
+            print(f"*********  round({x}) ****************")
             self._theta()
             dump_buffer(self.state.buffer)
             self._rho_pi()
             self._chi()
             self._iota(x)
-
 
     def _xorin(self, buffer: State):
         for i in range(0, len(buffer)):
